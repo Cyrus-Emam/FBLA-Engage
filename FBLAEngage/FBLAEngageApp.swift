8b0d980ae32362d636949805732097861ecfc5d1
Cyrus Emam <cyrus-emam@outlook.com>
2026-01-03 18:54:52 -0800

    Initial Commit

---
+++ FBLAEngage/FBLAEngageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBLAEngageApp.swift
+//  FBLAEngage
+//
+//  Created by Cyrus  Emam on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FBLAEngageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
